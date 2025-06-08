ed207794f29c2a05ffc731c620e907c5ebf94b27
Firstname Lastname <asakoshi@insightech.co.jp>
2025-06-08 12:41:34 +0900

    コミット１

---
+++ src/main/java/com/example/sb3bt/Sb3btApplication.java
@@ -0,0 +1,13 @@
+package com.example.sb3bt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb3btApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb3btApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sb3bt/Sb3btApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb3bt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb3btApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
