5c7e422a8ec1e2fd77d41183f248c1342e941ad2
Firstname Lastname <asakoshi@insightech.co.jp>
2025-07-13 16:44:58 +0900

    会員登録バッチ処理

---
+++ src/main/java/com/example/sb3bt/common/mapper/MembersMapper.java
@@ -0,0 +1,43 @@
+package main.java.com.example.sb3bt.common.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.sb3bt.common.entity.Members;
+
+@Mapper
+public interface MembersMapper {
+
+    /**
+     * 会員登録情報を1件登録するSQL。
+     *
+     * @param member 登録する会員情報
+     */
+    @Insert("INSERT INTO members(contract_id, member_id, product_id, card_number, registration_date) " +
+            "VALUES(#{contractId}, #{memberId}, #{productId}, #{cardNumber}, #{registrationDate})")
+    void insert(Members member);
+
+    /**
+     * 会員登録情報を複数件一括登録するSQL。
+     *
+     * @param list 登録する会員情報のリスト
+     */
+    @Insert({
+        "<script>",
+        "INSERT INTO members(contract_id, member_id, product_id, card_number, registration_date) VALUES ",
+        "<foreach collection='list' item='m' separator=','>",
+        "(#{m.contractId}, #{m.memberId}, #{m.productId}, #{m.cardNumber}, #{m.registrationDate})",
+        "</foreach>",
+        "</script>"
+    })
+    void bulkInsert(List<Members> list);
+
+    /**
+     * membersテーブルを全件削除するSQL。
+     */
+    @Delete("TRUNCATE TABLE members")
+    void truncate();
+}
